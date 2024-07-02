7137179a1534b1ab6c3476915c1022e1ed7b105d
Henrik Kjedsberg <henrik.kjeldsberg@live.no>
2024-07-02 19:55:11 +0200

    Export data from db to csv

---
+++ dump_data_to_csv.sql
@@ -0,0 +1,3 @@
+COPY recipes TO '/Users/henriakj/recipes.csv' WITH DELIMITER ',' CSV HEADER;
+COPY instructions TO '/Users/henriakj/instructions.csv' WITH DELIMITER ',' CSV HEADER;
+COPY driver TO '/Users/henriakj/driver.csv' WITH DELIMITER ',' CSV HEADER;
